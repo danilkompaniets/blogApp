f58ed9aada251cc76dcff2bed8601b196a2a8649
danilkompaniets <kompaniets1592925@gmail.com>
2024-01-27 22:18:36 +0300

    Article detail page completed

--- front-end/src/pages/articleDetail/ArticleDetailPage.jsx
+++ front-end/src/pages/articleDetail/ArticleDetailPage.jsx
@@ -1,36 +1,88 @@
-import React from 'react'
-import MainLayout from '../../components/MainLayout'
-import BreadCrumbs from '../../components/BreadCrumbs'
-import { images } from '../../constants'
+import React from "react";
+import MainLayout from "../../components/MainLayout";
+import BreadCrumbs from "../../components/BreadCrumbs";
+import { images } from "../../constants";
+import { Link } from "react-router-dom";
+import SugesstedPosts from "./container/SugesstedPosts";
 
 const BreadCrumbsData = [
-    { name: "Home", link: "/" },
-    { name: "blog", link: "/blog" },
-    { name: "Article Title", link: "/blog/1" },
-]
+  { name: "Home", link: "/" },
+  { name: "blog", link: "/blog" },
+  { name: "Article Title", link: "/blog/1" },
+];
+
+const postsData = [
+  {
+    _id: "1",
+    image: images.Post1Image,
+    title: "Help children get better education",
+    createdAt: "",
+  },
+  {
+    _id: "1",
+    image: images.Post1Image,
+    title: "Help children get better education",
+    createdAt: "",
+  },
+  {
+    _id: "1",
+    image: images.Post1Image,
+    title: "Help children get better education",
+    createdAt: "",
+  },
+];
+
+const tagsData = [
+  "Medical",
+  "Lifestyle",
+  "Learn",
+  "Healthy",
+  "Food",
+  "Diet",
+  "Education",
+];
 
 const ArticleDetailPage = () => {
-    return (
-        <MainLayout>
-            <section className='container mx-auto max-w-5xl flex flex-col px-5 py-5 '>
-                <article className='flex-1 '>
-                    <BreadCrumbs data={BreadCrumbsData} />
-                    <img src={images.Post1Image} alt="post image" className='rounded-xl w-full' />
-                    <Link to="/bolog?categoty=selectedCategory" className="text-primary text-sm font-roboto inline-blok mt-4 ">
-                        EDUCATION
-                    </Link>
-                    <h1 className='text-xl font-medium font-roboto mt-4 text-dark-hard'>
-                        Help Children get better education 
-                    </h1>
-                    <div className='mt-4 text-dark-soft'>
-                        <p className='leading-7'>
-                            
-                        </p>
-                    </div>
-                </article>
-            </section>
-        </MainLayout>
-    )
-}
+  return (
+    <MainLayout>
+      <section className="container mx-auto max-w-5xl flex lg:flex-row lg:gap-x-5 flex-col px-5 py-5 ">
+        <article className="flex-1 ">
+          <BreadCrumbs data={BreadCrumbsData} />
+          <img
+            src={images.Post1Image}
+            alt="post image"
+            className="rounded-xl w-full"
+          />
+          <Link
+            to="/bolog?categoty=selectedCategory"
+            className="text-primary text-sm font-roboto block  mt-4 md:text-base  lg:items-start   "
+          >
+            EDUCATION
+          </Link>
+          <h1 className="text-xl font-medium font-roboto mt-4 text-dark-hard md:text-[26px]">
+            Help Children get better education
+          </h1>
+          <div className="mt-4 text-dark-soft">
+            <p className="leading-7">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Egestas purus viverra accumsan in nis! nisi. Arcu cursus vitae
+              congue mauris rhoncus aenean vel elit scelerisque. n egestas erat
+              imperdiet sed euismod nisi porta lorem mollis. Morbi tristique
+              senectus et netus. Mattis pellentesque id nibh tortor id aliquet
+              lectus proin.
+            </p>
+          </div>
+        </article>
+        <SugesstedPosts
+          posts={postsData}
+          header={"Latest Article"}
+          tags={tagsData}
+          className={"mt-8 lg:mt-0 lg:max-w-xs  "}
+        />
+      </section>
+    </MainLayout>
+  );
+};
 
-export default ArticleDetailPage
+export default ArticleDetailPage;
---
+++ front-end/src/pages/articleDetail/container/SugesstedPosts.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { images } from "../../../constants";
+import { BsTags } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+const SugesstedPosts = ({ className, header, posts = [], tags }) => {
+  return (
+    <div
+      className={`w-full shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px] rounded-lg p-4  ${className}`}
+    >
+      <h2 className="font-roboto font-medium text-dark-hard md:text-xl">
+        {header}
+      </h2>
+      <div className="grid gap-y-5 mt-5 md:grid-cols-2 lg:grid-cols-1 md:gap-x-5  ">
+        {posts.map((item) => (
+          <div
+            key={item._id}
+            className="flex space-x-3 flex-nowrap items-center "
+          >
+            <img
+              src={item.image}
+              alt="laptop"
+              className="aspect-square object-cover rounded-lg w-1/5 "
+            />
+            <div className="text-sm font-roboto text-dark-hard font-medium ">
+              <h3 className="text-sm font-roboto text-dark-hard font-medium md:text-base lg:text-lg">
+                {item.title}
+              </h3>
+              <span className="text-xs opacity-60">
+                {new Date(item.createdAt).toLocaleDateString("en-US", {
+                  day: "numeric",
+                  month: "short",
+                  year: "numeric",
+                })}
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h2 className="font-roboto font-medium text-dark-hard mt-8 md:text-xl">
+          Tags
+        </h2>
+        <div className="flex flex-wrap gap-x-2 gap-y-2 mt-4 ">
+          {tags.map((item) => (
+            <Link
+              to={"/"}
+              className="inline-block rounded-md px-3 py-1.5 bg-primary font-roboto text-xs text-white md:text-sm"
+            >
+              {item}
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SugesstedPosts;
